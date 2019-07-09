9414c9dbcf86f598070407d4e81971021f3ede5e
foggybrains <dustin.keyes@gmail.com>
2019-07-09 10:22:02 -0600

    added API methods

--- lib/api.rb
+++ lib/api.rb
@@ -1,12 +1,27 @@
-class API
+require 'pry'
+require 'rest-client'
+require 'json'
+
+class Api
   def query
     api = RestClient.get("")
     @api = JSON.parse(api)
   end
 
-  def query_get_name
+  def self.adjectives_for(word)
+    api = RestClient.get("https://api.datamuse.com/words?rel_jjb=#{word}")
+    adjectives = JSON.parse(api)
+    adjectives.first(10).map do |adj|
+      adj["word"]
+    end
   end
-
-  def query_get_pronunciation
+  
+  def self.rhymes_with(word)
+    api = RestClient.get("https://api.datamuse.com/words?rel_rhy=#{word}")
+    rhymes = JSON.parse(api)
+    rhymes.first(15).map do |rhyme|
+      rhyme["word"]
+    end
   end
+  
 end
