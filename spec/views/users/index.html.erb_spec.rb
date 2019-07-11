045a5aedbd44ad9cedc7b94194225df2e58834e5
foggybrains <dustin.keyes@gmail.com>
2019-07-10 21:12:23 -0600

    3 tests

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -7,3 +7,27 @@ describe "Route to view" do
    expect(page.status_code).to eq(200)
  end
 end
+
+
+describe 'Welcome navigate' do
+    before do
+      @poem = Poem.create(poem: "yo yo, i love you boo")
+    end
+
+  it 'displays a link to the edit page' do 
+    visit poem_path(@poem.id)
+
+    expect(page).to have_link(poem_path(@poem))
+  end
+
+  # it 'displays a link to the artist page' do 
+  #   visit song_path(@song.id)
+
+  #   expect(page).to have_link(@artist.name, href: artist_path(@artist))
+  # end
+
+  # it 'to song pages' do
+  #   visit song_path(@song.id)
+  #   expect(page.status_code).to eq(200)
+  # end
+end
